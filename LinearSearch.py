3cf745c12cecb839ed0fc0d73a73caa70a14bb22
Vikas Shukla <vikas.shukla@insurgepartners.com>
2024-03-31 22:05:52 -0500

    LinearSearch

---
+++ LinearSearch.py
@@ -0,0 +1,17 @@
+# Linear Search
+
+array_ = [2, 5,1, 5, 8, 9]
+k = 3
+
+length = len(array_)
+
+for i in range(length):
+    if (array_[i] == k):
+        print(i)
+        
+print("-1")
+
+
+    
+    
+    
